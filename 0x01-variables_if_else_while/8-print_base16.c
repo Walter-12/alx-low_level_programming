a1cd2d7f37478c36da7e0e7e0f6e10668c190a51
Walter-12 <walterongondo12@gmail.com>
2023-09-13 15:33:26 -0700

    fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,12 +13,12 @@ int main(void)
 	char c;
 	int i;
 
-	for (i = 48; i<= 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar (i);
 	}
 
-	for (c= 'a'; c<= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar (c);
 	}
