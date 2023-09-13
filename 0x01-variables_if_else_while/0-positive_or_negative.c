6b5491291d03b6ddb0594161e57fe34931da36b3
Walter-12 <walterongondo12@gmail.com>
2023-09-13 15:08:37 -0700

    fix2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - determine if number is positive or negative
  *
- * Description: print whether number stored in variable n 
+ * Description: print whether number stored in variable n
  * is positive or negative
  *
  * Return: Always 0 (Success)
