1e66074734785039e4a8c72dbd28ee9f663ee176
Walter-12 <walterongondo12@gmail.com>
2023-09-29 17:04:27 -0700

    fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
